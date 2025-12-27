9493e7d3c156417c165331c20cafee976ce53815
Bastien-Antigravity <bastien.chanseau.bc@gmail.com>
2025-12-28 00:06:57 +0100

    Add doc comments to TcpHelloProfile

--- src/profiles/tcp_hello.go
+++ src/profiles/tcp_hello.go
@@ -2,13 +2,17 @@ package profiles
 
 import "github.com/Bastien-Antigravity/safe-socket/src/interfaces"
 
-// TcpHelloProfile is a pre-configured profile for TCP transport with Hello protocol.
+// TcpHelloProfile implements the SocketProfile interface for a TCP connection
+// that performs a handshake protocol upon connection.
 type TcpHelloProfile struct {
 	Name    string
 	Address string
 	Timeout int
 }
 
+// -----------------------------------------------------------------------------
+
+// NewTcpHelloProfile creates a new instance of a TCP profile with the Hello protocol.
 func NewTcpHelloProfile(name, address string, timeout int) *TcpHelloProfile {
 	return &TcpHelloProfile{
 		Name:    name,
@@ -17,22 +21,37 @@ func NewTcpHelloProfile(name, address string, timeout int) *TcpHelloProfile {
 	}
 }
 
+// -----------------------------------------------------------------------------
+
+// GetName returns the name assigned to this profile.
 func (p *TcpHelloProfile) GetName() string {
 	return p.Name
 }
 
+// -----------------------------------------------------------------------------
+
+// GetAddress returns the target network address.
 func (p *TcpHelloProfile) GetAddress() string {
 	return p.Address
 }
 
+// -----------------------------------------------------------------------------
+
+// GetTransport specifies the use of Framed TCP for this profile.
 func (p *TcpHelloProfile) GetTransport() interfaces.TransportType {
 	return interfaces.TransportFramedTCP
 }
 
+// -----------------------------------------------------------------------------
+
+// GetProtocol specifies the use of the SayHello handshake protocol.
 func (p *TcpHelloProfile) GetProtocol() interfaces.ProtocolType {
 	return interfaces.ProtocolSayHello
 }
 
+// -----------------------------------------------------------------------------
+
+// GetConnectTimeout returns the configured connection timeout.
 func (p *TcpHelloProfile) GetConnectTimeout() int {
 	return p.Timeout
 }
